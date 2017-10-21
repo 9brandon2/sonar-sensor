9d67d6c2d628a7e82e1de67d82cd5b9fc120acc3
Brandon Ojeda <Brandon.Ojeda@mitratech.com>
2017-10-21 12:31:13 -0500

    Added additional sesnsor changes

--- sonar_sensor/sonar_sensor.ino
+++ sonar_sensor/sonar_sensor.ino
@@ -12,6 +12,7 @@ const int echoPin = 10;
 long duration;
 int distanceCM;
 int distanceIN;
+int count;
 
 void setup() {
     pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
@@ -19,19 +20,28 @@ void setup() {
     Serial.begin(9600); // Starts the serial communication
 }
 void loop() {
+    
     // Clears the trigPin
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
     // Sets the trigPin on HIGH state for 10 micro seconds
     digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
+    delayMicroseconds(10000);
     digitalWrite(trigPin, LOW);
     // Reads the echoPin, returns the sound wave travel time in microseconds
     duration = pulseIn(echoPin, HIGH);
     // Calculating the distance
     distanceCM= duration*0.034/2;
     distanceIN= duration*0.0133/2;
-    // Prints the distance on the Serial Monitor
-    Serial.print("DistanceIN: ");
-    Serial.println(distanceIN);
+    if (distanceIN < 25){
+      count += 1;
+      if (count > 3){
+      // Prints the distance on the Serial Monitor
+        Serial.print("DistanceIN: ");
+        Serial.println(distanceIN);
+      }
+    }
+    else {
+      count = 0;
+    }
 }
